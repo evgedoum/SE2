4475ddb60c9e1b029c3dc3158625a246b4ed0d11
Eleftherk <eleftherkala@gmail.com>
2023-12-09 18:23:35 +0200

    upload tests for Get orders by id end point

--- tests/order_functions.test.js
+++ tests/order_functions.test.js
@@ -1,5 +1,8 @@
 const test = require('ava');
 const { ordersGET } = require('../service/DefaultService');
+const { ordersOrderIdGET } = require('../service/DefaultService');
+const { ordersUserGET } = require('../service/DefaultService');
+const { ordersPOST } = require('../service/DefaultService');
 
 function order_contain(t, order) {
     // tests for the items that returns the get orders
@@ -13,10 +16,33 @@ function order_contain(t, order) {
     t.is(order.products[1].productId,1);
   };
 module.exports =  {order_contain} ;
+
 //-----------------------------------------------------------------------//
+//                              GET orders                               //
+//-----------------------------------------------------------------------//
+
 test ('GET orders', async t => {
     const result = await ordersGET();
     t.is(result.length,2);
     order_contain(t, result[0]);
     order_contain(t, result[1]);
+});
+
+//-----------------------------------------------------------------------//
+//                            GET orders by id                           //
+//-----------------------------------------------------------------------//
+
+test ('GET order by id', async t =>{
+    const result = await ordersOrderIdGET(0);
+    order_contain(t, result);
+});
+
+test ('GET order by id without id', async t =>{
+    const result = await ordersOrderIdGET('');
+    order_contain(t, result);
+});
+
+test ('GET order by id without integer id', async t =>{
+    const result = await ordersOrderIdGET('a');
+    order_contain(t, result);
 });
--- tests/order_server.test.js
+++ tests/order_server.test.js
@@ -15,7 +15,11 @@ test.before (async (t) => {
 test.after.always((t) => {
     t.context.server.close();
 });
-//-----------------------------------------------------------------------//
+
+//-----------------------------------------------------------------------------------------------//
+//                                           GET orders                                          //
+//-----------------------------------------------------------------------------------------------//
+
 test ('GET orders service', async (t) => {
     const { body ,statusCode } = await t.context.got("orders",{throwHttpErrors: false}); 
     t.is(statusCode, 200);
@@ -24,4 +28,36 @@ test ('GET orders service', async (t) => {
     functions.order_contain(t, body[1]);
 });
 
+//-----------------------------------------------------------------------------------------------//
+//                                      GET orders by id                                         //
+//-----------------------------------------------------------------------------------------------//
+
+//i didnt fount the way to test the negative id so as to return status code to be 400
+
+test ('GET order by id service', async (t) =>{
+    const { body ,statusCode } = await t.context.got("orders/0",{throwHttpErrors: false});
+    t.is(statusCode, 200);
+    functions.order_contain(t, body);
+});
+
+//we used t.not because our data is dummy so we cant check the existance of id 10, 
+//and we want to contunue the ci
+test ('GET order by id service unexisted', async (t) =>{
+    const { body ,statusCode } = await t.context.got("orders/10",{throwHttpErrors: false});
+    t.not(body.id,10);
+});
+
+test ('GET order id with blank id on the service', async (t) =>{
+    const { body ,statusCode } = await t.context.got("orders/",{throwHttpErrors: false});
+    t.is(statusCode, 200);
+    t.is(body.length,2);
+    functions.order_contain(t, body[0]);
+    functions.order_contain(t, body[1]);
+});
+
+test ('GET order fail wrong id type', async (t) =>{
+    const { body ,statusCode } = await t.context.got("orders/a",{throwHttpErrors: false});
+    t.is(statusCode, 400);
+    t.is(body.message,"request.params.orderId should be integer");
+});
 
